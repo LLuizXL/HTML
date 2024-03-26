1d5d62948b3da6014389494163bcc0f015b2f71b
LuizG <luiz.mwlo76@gmail.com>
2024-03-26 10:16:39 -0300

    alterações dia 26/03

--- AULA02/js/script.js
+++ AULA02/js/script.js
@@ -1,3 +1,17 @@
 alerta = () => {
-    alert("Olá, sou um alerta! apesar de não alertar nada.")
-}
+  alert("Olá, sou um alerta! apesar de não alertar nada.");
+};
+
+const enviar = () => {
+  let nome = document.getElementById("nome").value;
+
+  document.getElementById(
+    "resposta"
+  ).innerHTML = `<h2>Olá ${nome}, formulário enviado com sucesso! </h2>`;
+
+  if (nome == "") {
+    alert("Bem vindo!");
+  } else {
+    alert(`Olá ${nome}, bem vindo!!!!`);
+  }
+};
